881bcc74092c8e7138de2fe2fcc284f2469374be
Sefa Can <fsefacan@gmail.com>
2020-02-14 22:45:34 +0300

    fix missing method in interface, upgrade 2.0 to netstandard2.1

--- src/NumberToWords/ITransformer.cs
+++ src/NumberToWords/ITransformer.cs
@@ -3,5 +3,7 @@ namespace NumberToWords
     public interface ITransformer
     {
         string ToWords(int value);
+
+        string ToCurrencyWords(decimal currency, string currencyCode);
     }
 }
